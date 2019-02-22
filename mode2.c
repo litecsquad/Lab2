6330d6ac4039bda58fa44130b7feefef2d700f25
galiolvera <47731380+galiolvera@users.noreply.github.com>
2019-02-22 00:41:51 -0500

    Update mode2.c

--- mode2.c
+++ mode2.c
@@ -1,4 +1,37 @@
-if(!PB0)
+//MODE 2 VARIABLES://
+
+int game_sequence[8][2] =  {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}};
+
+unsigned int blinks; // assigned with potentiometer for game_sequence array
+unsigned char led; // LED picked with PBs for game_sequence array
+unsigned char button; // determines what PB pushed to light up corresponding LED
+
+
+/**************
+FUNCTIONS USED IN MODE 2:
+TurnOff();
+blink_LED();
+***************/
+
+
+/*********  MAIN MODE 2 CODE  ****************/
+
+void main(void)
+{
+	Sys_Init();      // System Initialization
+        Port_Init();     // Initialize ports 2 and 3 
+        Interrupt_Init();
+        Timer_Init();    // Initialize Timer 0 
+	putchar(' ');    // the quote fonts may not copy correctly into SiLabs IDE
+	//ADC Initialization included later on
+	
+    TurnOff(); //MAKE SURE ALL LEDs turned off
+	// MODE 2 INSTRUCTIONS //
+    printf("\r\nMode 2 Selected");
+    printf("\r\nPlayer 1 creates game sequence for Player 2.\r\nPush any button to begin your sequence\r\n");
+
+    //CHOOSE FIRST LED IN SEQUENCE//
+    if(!PB0)
         {
             button = PB0;
             led = 0;
@@ -18,16 +51,23 @@ if(!PB0)
             button = PB3;
             led = 3;
 		}
-	for (turn = 0; turn < 7; ++turn) //goes on to build rest of the sequence
+	for (turn = 0; turn < 7; turn++) //goes on to build rest of the sequence (TURN ++ OR ++TURN???)
 	{
-		while (button) //when pushbutton released, light up corresponding LED for 1 second
+        counts = 0;
+        TR0 = 0;
+        TR0 = 1;
+        TMR0 = 0;
+
+        while (!button); 
+		while (button) //ONCE pushbutton released, light up corresponding LED for 1 second
         {
             blinks_LED(led, 1);
         }
 
         while(counts < 674) //wait 1 second for potentiometer to determine number of blinks
         {
-            while (PB0 & PB1 & PB2 & PB3) //if NO pushbutton is pressed, determine # of blinks with potentiometer
+            while (!PB0 && !PB1 && !PB2 && !PB3);
+            while (PB0 && PB1 && PB2 && PB3) //if NO pushbutton is pressed, determine # of blinks with potentiometer
             {
                 ADC_Init(); //initiate potentiometer, returns ADC1 from 0 - 255
                 (ADC1)%= 3;
@@ -50,7 +90,13 @@ if(!PB0)
             }
         }
 		
-		game[turn] = {led, blinks};
+	TurnOff();	
+        counts = 0;
+        TR0 = 0;
+        TR0 = 1;
+        TMR0 = 0;
+
+		game_sequence[turn] = {led, blinks};
 		
 		if (!PB0) //determine next LED in sequence
 		{
@@ -72,9 +118,14 @@ if(!PB0)
 			button = PB3;
 			led = 3;
 		}
+		TurnOff();
     }
-    while(counts < 674) //turn on all LEDs for one second to confirm that sequences has been created
+    /* END OF for LOOP*/
+    while(counts < 674) //turn on all LEDs for one second to confirm that sequences has been created.
     {
-        LED0 = 1; LED1 = 1; LED2 = 1; LED3 = 1;
+
+        LED0 = 0; LED1 = 0; LED2 = 0; LED3 = 0;
+
     }
+TurnOff();
 }
